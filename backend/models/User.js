6249a979a36eb93f050f34056cb1b8006e30a30d
dlesesne23 <davidplesesne@gmail.com>
2024-04-15 20:19:35 -0500

    added more user info

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    likedSongs: {type: [String], default: []},
+    playlists: {type: [String], default: []},
 })
 
 module.exports = mongoose.model('User', userSchema)
