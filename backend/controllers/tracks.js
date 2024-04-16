82c400730d76a4a1e9785177d1f3087d8ab0e003
wickhill <wickhill@gmail.com>
2024-04-15 22:10:36 -0400

    Tracks model and tracks controller added

--- backend/controllers/tracks.js
+++ backend/controllers/tracks.js
@@ -23,11 +23,27 @@ router.post("/", (req, res) => {
 });
 
 // SHOW - Returns a single track by ID as JSON
-router.get("/:spotifyId", (req, res) => {
-    db.Track.findById(req.params.spotifyId)
+router.get("/:id", (req, res) => {
+    db.Track.findById(req.params.id)
     .then((track) => {
         if(!track) res.status(404).json({ error: "Track not found" });
         else res.json(track);
     })
     .catch((err) => res.json({ error: err.message }));
 });
+
+// UPDATE - Updates a specific track and returns the updated track as JSON
+router.put("/:id", (req, res) => {
+    db.Track.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((track) => res.json(track))
+    .catch((err) => res.json({ error: err.message }));
+});
+
+// DELETE - Deletes a specific track and returns a success message
+router.delete("/:id", (req, res) => {
+    db.Track.findByIdAndDelete(req.params.id)
+    .then(() => res.json({ message: "Track successfully deleted" }))
+    .catch((err) => res.json({ error: err.message }));
+});
+
+module.exports = router;
