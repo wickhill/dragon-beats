55903e4175eebbe92ac51ef66644704c6fd87ff6
dlesesne23 <davidplesesne@gmail.com>
2024-04-20 12:05:02 -0500

    Set up Login/Signup auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from 'react'
+import { Route, Routes } from "react-router-dom"
+
+import AuthPage from "./pages/AuthPage"
 
 function App() {
+    const [user, setUser] = useState(null)
+
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/auth" element={<AuthPage setUser={setUser} />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
