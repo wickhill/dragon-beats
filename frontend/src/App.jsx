5919086dfd176e6dbb6337d1a91c827d6a40c8c7
dlesesne23 <davidplesesne@gmail.com>
2024-04-22 21:33:10 -0500

    signin component works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router-dom"
 import Auth from "./components/Auth"
 import Spotify from "./components/Spotify"
+import Signin from "./components/Signin"
 
 function App() {
-    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const hash = window.location.hash
+        console.log(hash)
+    })
+
     return (
         <>
         <h1>Dragon Beats App!</h1>
         <div>
-            <Navbar />
-            {
-            <Auth user={user} setUser={setUser} />
-            }
-            <Footer />
+            <Signin />
         </div>
         </>
     )
