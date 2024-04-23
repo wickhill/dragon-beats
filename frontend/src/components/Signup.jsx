b6572a24a7f2d292d88c5bb833f5eb124d8ee674
dlesesne23 <davidplesesne@gmail.com>
2024-04-22 21:44:28 -0500

    added signup component

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const Signup = ({ onSignup }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    
+    // Here you would typically perform sign-up logic
+    // For demonstration, let's assume sign-up is successful
+    const response = await fetch('http://localhost:3000/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      });
+      const accessToken = ' ';
+      const res = await response.json()
+      console.log(res)
+      // Call the onSignIn function passed from the parent component
+      onSignup(accessToken);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up for Your Spotify Account</h2>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
