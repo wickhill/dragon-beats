1f760d100c3865aaed7388dac50ff0a3ad9ae8f2
dlesesne23 <davidplesesne@gmail.com>
2024-04-25 21:20:28 -0500

    styled login and signup components

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import img3 from '../assets/player.jpeg'
+import { FaSpotify } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
 
 const Signin = ({ onSignin }) => {
     const [username, setUsername] = useState('');
@@ -27,13 +30,18 @@ const Signin = ({ onSignin }) => {
   navigate("/")
     };
     return (
-      <div className="pt-[200px]">
-        <div className="max-w-[1400px] my-0 mx-auto">
-        <h2>Sign In</h2>
-        <form onSubmit={handleSignin}>
-          <div>
+      <div className="pt-[75px] relative w-full h-screen bg-zinc-700/90">
+            <img className="absolute w-full h-full object-cover mix-blend-overlay" src={img3} alt='' />
+        <div className="flex justify-center items-center h-full">
+        <form className= "max-w-[400px] w-full mx-auto bg-white p-8" onSubmit={handleSignin}>
+        <h2 className="text-4xl font-bold text-center py-6">Sign In</h2>
+        <div className="flex justify-between px-6 py-8">
+        <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center"><FaSpotify />Spotify</p>
+        <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center"><FaGithub />Github</p>
+      </div>
+          <div className="flex flex-col px-1 py-2 mb-1">
             <label htmlFor="username">Username:</label>
-            <input
+            <input className="border relative bg-gray-100 p-1"
               type="text"
               id="username"
               value={username}
@@ -41,9 +49,9 @@ const Signin = ({ onSignin }) => {
               required
             />
           </div>
-          <div>
+          <div className="flex flex-col px-1 py-2 mb-1">
             <label htmlFor="password">Password:</label>
-            <input
+            <input className="border relative bg-gray-100 p-1"
               type="password"
               id="password"
               value={password}
@@ -51,7 +59,7 @@ const Signin = ({ onSignin }) => {
               required
             />
           </div>
-          <button type="submit">Sign In</button>
+          <button className="w-full py-3 mt-4 bg-gray-400 hover:bg-gray-300 relative" type="submit">Sign In</button>
         </form>
       </div>
       </div>
